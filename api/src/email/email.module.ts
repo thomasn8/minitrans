9f4f8cecf674c3b6f72cb45ca1b5dcd165cc2c63
Thomas Nanchen <thomasnanchen@hotmail.com>
2023-04-29 11:56:14 +0200

    problem with emailmodule dependencies

--- api/src/email/email.module.ts
+++ api/src/email/email.module.ts
@@ -1,38 +1,115 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { EmailService } from './email.service';
 import { join } from 'path';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as nodemailer from 'nodemailer';
 
+@Global()
 @Module({
-  imports: [
-    MailerModule.forRoot({
-      // transport: 'smtps://user@example.com:topsecret@smtp.example.com',
-      // or
-      transport: {
-        // host: 'smtp.example.com',
-        // host: 'localhost',
-        host: 'back',
-        port: 587,
-        secure: false, // upgrade later with STARTTLS      
-        auth: {
-          user: 'user@example.com',
-          pass: 'topsecret',
-        },
-      },
-      defaults: {
-        from: '"No Reply" <noreply@example.com>',
-      },
-      template: {
-        dir: join(__dirname, 'templates'),
-        adapter: new HandlebarsAdapter(),
-        options: {
-          strict: true,
-        },
-      },
-    }),
-  ],
+  imports: [],
   providers: [EmailService],
   exports: [EmailService],
 })
 export class EmailModule {}
+
+
+// @Global()
+// @Module({
+//     imports: [
+//       MailerModule.forRootAsync({
+//         // imports: [ConfigModule], // import module if not enabled globally
+//         useFactory: async (config: ConfigService) => ({
+//           // transport: config.get("MAIL_TRANSPORT"),
+//           // or
+//           transport: {
+//             host: config.get('MAIL_HOST'),
+//             port: config.get('MAIL_PORT'),
+//             secure: false,
+//             auth: {
+//               user: config.get('MAIL_USER'),
+//               pass: config.get('MAIL_PASSWORD'),
+//             },
+//           },
+//           defaults: {
+//             from: `"No Reply" <${config.get('MAIL_FROM')}>`,
+//           },
+//           template: {
+//             dir: join(__dirname, 'templates'),
+//             adapter: new HandlebarsAdapter(),
+//             options: {
+//               strict: true,
+//             },
+//           },
+//         }),
+//         inject: [ConfigService],
+//       }),
+//     ],
+//     providers: [EmailService],
+//     exports: [EmailService],
+//   })
+//   export class EmailModule {}
+
+
+// @Global()
+// @Module({
+//   imports: [
+//     MailerModule.forRoot({
+//       transport: {
+//         host: process.env.MAIL_HOST,
+//         // port: process.env.MAIL_PORT ? parseInt(process.env.MAIL_PORT) : 587,
+//         port: 587,
+//         secure: false, // upgrade later with STARTTLS
+//         auth: {
+//           user: process.env.MAIL_USER,
+//           pass: process.env.MAIL_PASSWORD,
+//         },
+//       },
+//       defaults: {
+//         from: '"No Reply" <noreply@example.com>',
+//       },
+//       template: {
+//         dir: join(__dirname, 'templates'),
+//         adapter: new HandlebarsAdapter(),
+//         options: {
+//           strict: true,
+//         },
+//       },
+//     }),
+//   ],
+//   providers: [EmailService],
+//   exports: [EmailService],
+// })
+// export class EmailModule {}
+
+
+// @Global()
+// @Module({
+//   imports: [
+//     MailerModule.forRoot({
+//       transport: {
+//         service: 'gmail',
+//         auth: {
+//           user: process.env.MAIL_USER,
+//           pass: process.env.MAIL_PASSWORD,
+//           clientId: process.env.CLIENT_ID,
+//           clientSecret: process.env.CLIENT_SECRET,
+//         }
+//       },
+//       defaults: {
+//         from: '"No Reply" <noreply@example.com>',
+//       },
+//       template: {
+//         dir: join(__dirname, 'templates'),
+//         adapter: new HandlebarsAdapter(),
+//         options: {
+//           strict: true,
+//         },
+//       },
+//     }),
+//   ],
+//   providers: [EmailService],
+//   exports: [EmailService],
+// })
+// export class EmailModule {}
