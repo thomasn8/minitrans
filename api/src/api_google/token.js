1a6825e29009dbafa2a2737655dcd1c164b7a2f3
Thomas Nanchen <thomasnanchen@hotmail.com>
2023-05-10 09:02:56 +0200

    git filter-repo

--- api/src/api_google/token.js
+++ api/src/api_google/token.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const credentials = require('./credentials.json');
 
 // Replace with the code you received from Google
-const code = '4/0AbUR2VOQbUDLLQ1Mwlb2lY3U1hF1U2uVWHSrKdXhOR_IeJlAEvw2cbPTX7Gj8Q2oGn6PWg';
+const code = '...';
 const { client_secret, client_id, redirect_uris } = credentials.web;
 const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
